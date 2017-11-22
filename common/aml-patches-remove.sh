bddc86ffa4ca0ec53a83f658e4bddb42a9966d08
Zackptg5 <zackptg5@comcast.net>
2017-11-22 15:13:45 -0500

    Autodetect all conf/pol/mix files

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -1,6 +1,4 @@
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
-    sed -i '/jamesdsp {/,/}/d' $AMLPATH$CFG
-    sed -i '/jdsp {/,/}/d' $AMLPATH$CFG
-  fi
+for FILE in ${CFGS}; do
+  sed -i '/jamesdsp {/,/}/d' $AMLPATH$FILE
+  sed -i '/jdsp {/,/}/d' $AMLPATH$FILE
 done
--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -1,7 +1,7 @@
 ui_print "    Patching existing audio_effects files..."
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ] && [ ! "$(cat $AMLPATH$CFG | grep ' jamesdsp {')" ]; then
-    sed -i 's/^effects {/effects {\n  jamesdsp {\n    library jdsp\n    uuid f27317f4-c984-4de6-9a90-545759495bf2\n  }/g' $AMLPATH$CFG
-    sed -i 's/^libraries {/libraries {\n  jdsp {\n    path \/system\/lib\/soundfx\/libjamesdsp.so\n  }/g' $AMLPATH$CFG
+for FILE in ${CFGS}; do
+  if [ ! "$(cat $AMLPATH$FILE | grep ' jamesdsp {')" ]; then
+    sed -i 's/^effects {/effects {\n  jamesdsp {\n    library jdsp\n    uuid f27317f4-c984-4de6-9a90-545759495bf2\n  }/g' $AMLPATH$FILE
+    sed -i 's/^libraries {/libraries {\n  jdsp {\n    path \/system\/lib\/soundfx\/libjamesdsp.so\n  }/g' $AMLPATH$FILE
   fi
 done
