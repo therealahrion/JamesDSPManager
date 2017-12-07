8a6800930a0b8b901e866a3706b3190a0ae44898
Zackptg5 <zackptg5@comcast.net>
2017-12-07 17:55:28 -0500

    Bring back oreonew stuff

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -1,8 +1,8 @@
 TIMEOFEXEC=3
 if [ "$ABI" == "arm" ]; then
-  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesdsp.so $UNITY$SYS/lib/soundfx/libjamesdsp.so
-  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesDSPImpulseToolbox.so $UNITY$SYS/lib/libjamesDSPImpulseToolbox.so
+  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesdsp.so $UNITY$LIBDIR/lib/soundfx/libjamesdsp.so
+  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesDSPImpulseToolbox.so $UNITY$LIDIR/lib/libjamesDSPImpulseToolbox.so
 else
-  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesdsp.so $UNITY$SYS/lib/soundfx/libjamesdsp.so
-  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesDSPImpulseToolbox.so $UNITY$SYS/lib/libjamesDSPImpulseToolbox.so
+  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesdsp.so $UNITY$LIBDIR/lib/soundfx/libjamesdsp.so
+  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesDSPImpulseToolbox.so $UNITY$LIBDIR/lib/libjamesDSPImpulseToolbox.so
 fi
