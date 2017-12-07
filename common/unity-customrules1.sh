51b7b3df807301b75b5a8ec74a4f0bf82a5a7acd
Zackptg5 <zackptg5@comcast.net>
2017-12-07 18:05:09 -0500

    add back oreonew stuff

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -1,10 +1,10 @@
 TIMEOFEXEC=3
 if [ "$ABI" == "arm" ]; then
-  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesdsp.so $UNITY$SYS/lib/soundfx/libjamesdsp.so
-  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesDSPImpulseToolbox.so $UNITY$SYS/lib/libjamesDSPImpulseToolbox.so
+  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesdsp.so $UNITY$LIBDIR/lib/soundfx/libjamesdsp.so
+  $CP_PRFX $INSTALLER/custom/lib/arm/libjamesDSPImpulseToolbox.so $UNITY$LIDIR/lib/libjamesDSPImpulseToolbox.so
 else
-  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesdsp.so $UNITY$SYS/lib/soundfx/libjamesdsp.so
-  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesDSPImpulseToolbox.so $UNITY$SYS/lib/libjamesDSPImpulseToolbox.so
+  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesdsp.so $UNITY$LIBDIR/lib/soundfx/libjamesdsp.so
+  $CP_PRFX $INSTALLER/custom/lib/x86/libjamesDSPImpulseToolbox.so $UNITY$LIBDIR/lib/libjamesDSPImpulseToolbox.so
 fi
 # App only works when installed normally to data in oreo
 if [ $API -ge 26 ]; then
