d7a371b82a088b1475156ba5a611d34f5eaac935
Zackptg5 <zackptg5@comcast.net>
2017-12-07 00:26:06 -0500

    Add xml cfg support, make xml separate from conf variables

--- common/aml-patches-remove.sh
+++ common/aml-patches-remove.sh
@@ -2,6 +2,10 @@ for FILE in ${CFGS}; do
   sed -i '/jamesdsp {/,/}/d' $AMLPATH$FILE
   sed -i '/jdsp {/,/}/d' $AMLPATH$FILE
 done
+for FILE in ${CFGSXML}; do
+  sed -i '/jamesdsp/d' $AMLPATH$FILE
+  sed -i '/jdsp/d' $AMLPATH$FILE
+done
 # if [ ! -z $XML_PRFX ]; then
   # # Enter xmlstarlet logic here
 # fi
