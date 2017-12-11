7d50145b71eef68738eaa68cc80b29dc8ec46e59
Zackptg5 <zackptg5@comcast.net>
2017-12-11 08:26:48 -0500

    Have bootscript install jdsp in oreo

--- common/service.sh
+++ common/service.sh
@@ -1,3 +1,4 @@
-pm install /storage/emulated/0/.jdsptempdonotdelete/JamesDSPManager.apk
-rm -rf /storage/emulated/0/.jdsptempdonotdelete
+sleep 15
+pm install /data/media/0/.jdsptempdonotdelete/JamesDSPManager.apk
+rm -rf /data/media/0/.jdsptempdonotdelete
 rm -f $0
