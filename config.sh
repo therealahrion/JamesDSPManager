11cb98e1627eef7e974a4024582df017272cebbc
Zackptg5 <zackptg5@comcast.net>
2018-02-18 11:02:00 -0500

    Fix paths for magisk mirrors

--- config.sh
+++ config.sh
@@ -46,8 +46,11 @@ DYNAMICOREO=true
 # Custom Variables - Keep everything within this function
 unity_custom() {
   if $MAGISK && $BOOTMODE; then ORIGDIR="/sbin/.core/mirror"; else ORIGDIR=""; fi
-  CFGS="${CFGS} $(find -L /system -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
-  if $BOOTMODE; then SDCARD=/storage/emulated/0; else SDCARD=/data/media/0; fi
+  if [ -L /system/vendor ]; then
+    CFGS="$(find /system /vendor -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
+  else  
+    CFGS="$(find /system -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
+  fi  if $BOOTMODE; then SDCARD=/storage/emulated/0; else SDCARD=/data/media/0; fi
 }
 
 ##########################################################################################
